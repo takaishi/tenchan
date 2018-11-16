98c375bfdc1c171a0b515f6756b403c7004dfb77
r_takaishi <ryo.takaishi.0@gmail.com>
2018-11-16 21:35:44 +0900

    view channel list filtered config file

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/takaishi/tenchan
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/gorilla/websocket v1.4.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/nlopes/slack v0.4.0
--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/joho/godotenv"
 	"github.com/nlopes/slack"
 	"github.com/urfave/cli"
 	"log"
 	"os"
+	"regexp"
 )
+
+type Config struct {
+	CTypes []CType `toml:"ctype"`
+}
+
+type CType struct {
+	Name  string `toml:"name"`
+	Match string `toml:"match"`
+}
 
 func main() {
 	app := cli.NewApp()
@@ -24,13 +35,24 @@ func main() {
 		}
 		slack_token := os.Getenv("SLACK_TOKEN")
 
+		var config Config
+		_, err = toml.DecodeFile(c.String("config"), &config)
+		if err != nil {
+			return err
+		}
+
 		api := slack.New(slack_token)
 		channels, err := api.GetChannels(true)
 		if err != nil {
 			return err
 		}
-		for _, channel := range channels {
-			fmt.Printf("%s\n", channel.Name)
+		for _, ctype := range config.CTypes {
+			for _, channel := range channels {
+				r := regexp.MustCompile(ctype.Match)
+				if r.Match([]byte(channel.Name)) {
+					fmt.Printf("%s\n", channel.Name)
+				}
+			}
 		}
 
 		return nil
